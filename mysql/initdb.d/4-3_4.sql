96f54753077d10792f74db2b4d1fda617c74b1ec
unknown <riku20020801@gmail.com>
2023-12-26 21:21:07 +0900

    4-3.4を修正

--- mysql/initdb.d/4-3_4.sql
+++ mysql/initdb.d/4-3_4.sql
@@ -1,7 +1,7 @@
 select x.業者番号 
 from 供給 as x, 供給 as y 
-where x.部品番号 = 5 
+where x.部品番号 = 1
     and y.業者番号 = 3 
-    and y.部門番号 = 7 
-    and y.部品番号 = 5 
+    and y.部門番号 = 2 
+    and y.部品番号 = 1 
     and x.単価 < y.単価;
